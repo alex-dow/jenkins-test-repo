2419e407ece3b19ffb17adb3eacf2d370b1b2705
alex-dow <adow@psikon.com>
2017-07-15 02:41:45 -0400

    Removed some junk and added some more tests

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,8 +1,17 @@
 from jtr import jtr
+import time
 
 def test_add():
     a = 1
     b = 2
 
     assert jtr._add(a, b) == 3
+
+def test_sub():
+    a = 3
+    b = 2
+
+    time.sleep(30)
+
+    assert jtr._sub(a, b) == 1
 
